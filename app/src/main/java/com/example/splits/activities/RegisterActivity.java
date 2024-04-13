67cbf435c14ad2938b945be249aff12e78e1ceb4
Dipendra Nath <dipen.2052@gmail.com>
2024-04-12 20:50:04 -0400

    register user form modified with register on click listener

--- app/src/main/java/com/example/splits/activities/RegisterActivity.java
+++ app/src/main/java/com/example/splits/activities/RegisterActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.splits.R;
 import com.example.splits.databinding.ActivityRegisterBinding;
+import com.example.splits.models.User;
+import com.example.splits.services.UserService;
+import com.example.splits.utilities.DatabaseHelper;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
     ActivityRegisterBinding activityRegisterBinding;
+    DatabaseHelper databaseHelper;
+    UserService userService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +24,46 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         activityRegisterBinding = ActivityRegisterBinding.inflate(getLayoutInflater());
         View view = activityRegisterBinding.getRoot();
         setContentView(view);
+
+//        set dbhelper and service
+        databaseHelper = new DatabaseHelper(this);
+        userService = new UserService(databaseHelper);
+//        set click listeners
         activityRegisterBinding.registerButton.setOnClickListener(this);
         activityRegisterBinding.bTHbutton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+
+
         if(view.getId() == activityRegisterBinding.bTHbutton.getId()){
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+            String name = activityRegisterBinding.nameField.getText().toString();
+            String email = activityRegisterBinding.emailField.getText().toString();
+            String password = activityRegisterBinding.passwordField.getText().toString();
+            String confirmPassword = activityRegisterBinding.confirmPasswordField.getText().toString();
+            if(password.equals(confirmPassword)){
+                User user = new User();
+                user.setName(name);
+                user.setEmail(email);
+                user.setPassword(password);
+                User newUser = userService.addUser(user);
+//                show toast to user
+                if(newUser == null){
+                    Toast.makeText(this, "Failed to add User", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    Toast.makeText(this, newUser.getName() + " added successfully", Toast.LENGTH_SHORT).show();
+                }
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+            }
+            else {
+                Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+            }
+
         }
-        if(view.getId() == activityRegisterBinding.registerButton.getId()){
+        else if(view.getId() == activityRegisterBinding.registerButton.getId()){
             Intent intent = new Intent(this, HomeActivity.class);
             startActivity(intent);
         }
